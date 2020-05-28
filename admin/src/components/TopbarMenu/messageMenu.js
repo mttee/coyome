51aa6db581a066612e5de7ee55c1249380a016db
Minh Tri <minhtri2497@gmail.com>
2020-05-28 23:40:11 +0700

    Create topbar for admin reactjs

---
+++ admin/src/components/TopbarMenu/messageMenu.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { DropdownButton, Dropdown, Badge } from "react-bootstrap/";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
+export default class messageMenu extends Component {
+  render() {
+    return (
+      <DropdownButton
+        id="dropdown-basic-button"
+        title={
+          <div>
+            <FontAwesomeIcon icon={faEnvelope} />
+            <Badge pill variant="primary">
+              20
+            </Badge>{" "}
+          </div>
+        }
+      >
+        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+      </DropdownButton>
+    );
+  }
+}
---
+++ admin/src/components/TopbarMenu/searchMenu.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { InputGroup, FormControl, Button, DropdownButton, Dropdown} from "react-bootstrap/";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
+export default class searchMenu extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="search-desktop">
+          <InputGroup className="nav-search">
+            <FormControl placeholder="Search..." />
+            <InputGroup.Append>
+              <Button variant="outline-secondary">
+                <FontAwesomeIcon icon={faSearch} />
+              </Button>
+            </InputGroup.Append>
+          </InputGroup>
+        </div>
+
+        <div className="search-mobile">
+          <DropdownButton
+            id="dropdown-basic-button"
+            title={
+              <FontAwesomeIcon icon={faSearch} />
+            }
+          >
+            <InputGroup className="nav-search-mobile">
+            <FormControl placeholder="Search..." />
+            <InputGroup.Append>
+              <Button variant="outline-secondary">
+                <FontAwesomeIcon icon={faSearch} />
+              </Button>
+            </InputGroup.Append>
+          </InputGroup>
+          </DropdownButton>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
