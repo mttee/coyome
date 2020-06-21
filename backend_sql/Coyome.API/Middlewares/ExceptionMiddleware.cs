bd47e2ab2a12874423972df43ae165ce25b050fc
Minh Tri <minhtri2497@gmail.com>
2020-06-21 21:16:45 +0700

    style Login form add create new backend api use sql

---
+++ backend_sql/Coyome.API/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,50 @@
+﻿using Coyome.Core.CustomExceptions;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Coyome.API.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate next;
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            var code = HttpStatusCode.InternalServerError; // 500 if unexpected
+
+            if (ex is MethodAccessException) code = HttpStatusCode.NotFound;
+            else if (ex is UnauthorizedAccessException) code = HttpStatusCode.Unauthorized;
+            else if (ex is ForbiddenException) code = HttpStatusCode.Forbidden;
+            else if (ex is BadRequestException)code = HttpStatusCode.BadRequest;
+
+            var result = JsonConvert.SerializeObject(new { 
+                statusCode = context.Response.StatusCode,
+                error = ex.Message 
+            });
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)code;
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
