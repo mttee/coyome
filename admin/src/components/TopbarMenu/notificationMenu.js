58757744920f2fef05a01ea96b1b72902e82ea32
Minh Tri <minhtri2497@gmail.com>
2020-07-15 17:34:52 +0700

    update topbar notification

--- admin/src/components/TopbarMenu/notificationMenu.js
+++ admin/src/components/TopbarMenu/notificationMenu.js
@@ -3,6 +3,15 @@ import { DropdownButton, Dropdown} from "react-bootstrap/";
 import Badge from '@material-ui/core/Badge';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell } from "@fortawesome/free-solid-svg-icons";
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import LinkIcon from '@material-ui/icons/Link';
+import DateRangeIcon from '@material-ui/icons/DateRange';
+import SettingsIcon from '@material-ui/icons/Settings';
+import MailIcon from '@material-ui/icons/Mail';
 
 export default class notificationMenu extends Component {
   render() {
@@ -20,9 +29,51 @@ export default class notificationMenu extends Component {
           Notifications
           <span>10</span>
         </div>
-        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+        <Dropdown.Item href="#/action-1" className="notification-item">
+          <ListItem>
+          <ListItemAvatar>
+          <Avatar variant="rounded">
+            <LinkIcon />
+          </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Launch Admin" secondary="Just see my new admin!" />
+        </ListItem>
+        </Dropdown.Item>
+
+        <Dropdown.Item href="#/action-1" className="notification-item">
+          <ListItem>
+          <ListItemAvatar>
+          <Avatar variant="rounded">
+            <DateRangeIcon />
+          </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Event Today" secondary="Just a reminder that you have event." />
+        </ListItem>
+        </Dropdown.Item>
+
+        <Dropdown.Item href="#/action-1" className="notification-item">
+          <ListItem>
+          <ListItemAvatar>
+          <Avatar variant="rounded">
+            <SettingsIcon />
+          </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Settings" secondary="You can customize this template as you want." />
+        </ListItem>
+        </Dropdown.Item>
+
+        <Dropdown.Item href="#/action-1" className="notification-item">
+          <ListItem>
+          <ListItemAvatar>
+          <Avatar variant="rounded">
+            <MailIcon />
+          </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Check Email" secondary="Just check my admin!" />
+        </ListItem>
+        </Dropdown.Item>
+
+        <a href="#/notification" className="notification-bottom align-self-end">View All</a>
       </DropdownButton>
     );
   }
