29169c0f55e2b4a603b8cd2e630152113d01abbf
Minh Tri <minhtri2497@gmail.com>
2020-07-23 17:45:49 +0700

    create new calendar

--- admin/src/routes.js
+++ admin/src/routes.js
@@ -1,9 +1,11 @@
 import Dashboard from "./containers/Dashboard/Dashboard"
 import Profile from "./containers/ProfileManager/Profile"
+import Calendar from "./containers/Calendar/Schedule"
 
 const routes = [
     {path: "/", exact: true, name: "Dashboard", component: Dashboard},
-    {path: "/profile", exact:true, name: "Profile", component: Profile}
+    {path: "/profile", exact:true, name: "Profile", component: Profile},
+    {path: "/calendar", exact:true, name: "Calendar", component: Calendar},
 ]
 
 export default routes;
